a9740cda978b4be85db0007ec4f68103dc7e7901
Bharathi Natarajan <bharathi_n@msn.com>
2021-10-06 00:07:25 +0530

    add new features, express dynamic routes, lodash,

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,22 +17,22 @@ app.use(express.static("public"));
 let posts = [];
 
 app.get('/', (req, res) => {
-  res.render('home', {homeContent: homeStartingContent});
-})
+  res.render('home', {homeContent: homeStartingContent, posts: posts});
+});
 
 
 app.get('/about', (req, res) => {
   res.render('about', {aboutContent: aboutContent});
-})
+});
 
 app.get('/contact', (req, res) => {
   res.render('contact', {contactContent: contactContent});
-})
+});
 
 
 app.get('/compose', (req, res) => {
   res.render('compose');
-})
+});
 
 app.post('/compose', (req, res) => {
   const post = {
@@ -40,10 +41,26 @@ app.post('/compose', (req, res) => {
   };
 
   posts.push(post);
-  console.log(posts);
   res.redirect('/');
 
-})
+});
+
+app.get('/posts/:postTitle', (req, res) => {
+  var postTitle = _.lowerCase(req.params.postTitle);
+  posts.forEach((post) => {
+    var savedTitle = _.lowerCase(post.title);
+    if(savedTitle === postTitle) {
+
+      var title = savedTitle;
+      var content = post.content;
+
+      res.render('post', {title: title, content: content});
+    }
+    else {
+      res.render('404');
+    }
+  });
+});
 
 
 
